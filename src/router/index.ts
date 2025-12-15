2e995626d4e43c723783e2a1258d7363ecf5b0f1
julien <garciajulien537@gmail.com>
2025-12-15 09:00:28 +0100

    Switch to Hash History for GH Pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProjectsView from "@/views/ProjectsView.vue"
 import ProjectDetailView from '@/views/ProjectDetailView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
